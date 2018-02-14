345a3d3ee2812a345b8dd157490bcd7895b527a1
pjury <pjury@protonmail.com>
2018-02-13 20:39:53 -0500

    added decent logging and rewrote tweet aggregator to ignore retweets

--- summon.py
+++ summon.py
@@ -7,9 +7,24 @@ import pickle
 import re
 from configparser import ConfigParser
 import logging
-from datetime import datetime
 
-def summon():
+
+def load_persistent(pickle_file):
+    if not os.path.isfile(pickle_file):
+        replied_comments = []
+    else:
+        replied_comments = pickle.load(open(pickle_file, 'rb'))
+
+    return replied_comments
+
+def load_last_tweet(pickle_file):
+    if not os.path.isfile(pickle_file):
+        last_tweet = ""
+    else:
+        last_tweet = pickle.load(open(pickle_file, 'rb'))
+    return last_tweet
+
+def get_tweet():
     config = ConfigParser()
     config.read('twitter.ini')
 
@@ -18,69 +33,67 @@ def summon():
     TOKEN_KEY = config.get('api_settings', 'token_key')
     TOKEN_SECRET = config.get('api_settings', 'token_secret')
 
-    twitter_auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    twitter_auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
-    twitter_api = tweepy.API(twitter_auth)
-    lesser_bot = twitter_api.get_user("ebooks_goetia")
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(TOKEN_KEY, TOKEN_SECRET)
+    api = tweepy.API(auth)
+    lesser_bot = api.get_user("ebooks_goetia")
+    last_tweet = load_last_tweet('last_tweet.p')
+    if last_tweet:
+        timeline = api.user_timeline(lesser_bot.id, max_id=last_tweet)
+        timeline = timeline[1::]
+    else:
+        timeline = api.user_timeline(lesser_bot.id)
+
+    for tweet in timeline:
+        if "RT" not in tweet.text:
+            logging.info("suitable tweet ({0}) found".format(tweet.id))
+            yield tweet
+        else:
+            pass
+
+def parse_tweet(tweet):
+    image_url = tweet.entities['media'][0]['media_url']
+    ascii_tweet = tweet.text.encode("ascii", "ignore")
+    ascii_tweet = re.sub(r"http\S+", "", ascii_tweet)
+    name = "{0} {1}".format(ascii_tweet.split(' ')[2],
+                            ascii_tweet.split(' ')[3].replace(',', ''))
+    attributes = [x.strip() for x in ascii_tweet.split("\n")[1::]]
+    attributes = ", ".join(attributes)
+
+    reply_str = "You have summoned [{0}]({1}) Demon of: {2}"
+    logging.info("reply parsed: {0}".format(reply_str))
+    reply = reply_str.format(name, image_url, attributes)
+    return reply
+
+
+def summon():
 
     reddit = praw.Reddit('bot1')
     subreddit = reddit.subreddit("test")
+    tweets = get_tweet()
+    replied_comments = load_persistent('replied_comments.p')
 
     for comment in subreddit.stream.comments():
-            replied_comments, last_tweet = load_persistent()
-            search = re.search("/u/summondemon", comment.body, re.IGNORECASE)
+        search = re.search("/u/summondemon", comment.body, re.IGNORECASE)
 
-            if comment.id not in replied_comments and search:
-                if last_tweet:
-                    timeline = twitter_api.user_timeline(lesser_bot.id, max_id=last_tweet)
-                    timeline = timeline[1::]
-                else:
-                    timeline = twitter_api.user_timeline(lesser_bot.id)
+        if comment.id not in replied_comments and search:
+            tweet = next(tweets)
+            reply = parse_tweet(tweet)
+            logging.info(reply)
+            comment.reply(reply)
+            logging.info("setting {0} as last tweet".format(tweet.id))
+            last_tweet = tweet.id
+            logging.info("adding {0} to replied comments".format(comment.id))
+            replied_comments.append(comment.id)
+            pickle.dump(last_tweet, open('last_tweet.p', 'wb'))
+            pickle.dump(replied_comments, open('replied_comments.p', 'wb'))
+            logging.info("persistent data saved")
 
-                tweet = timeline.pop()
-                image_url = tweet.entities['media'][0]['media_url']
-                ascii_tweet = tweet.text.encode("ascii", "ignore")
-                ascii_tweet = re.sub(r"http\S+", "", ascii_tweet)
-                #TODO This formatting sometimes throws an index error
-                #TODO Investigate cases where the index 3 is not present
-                name = "{0} {1}".format(ascii_tweet.split(' ')[2],
-                                        ascii_tweet.split(' ')[3].replace(',', ''))
-                attributes = [x.strip() for x in ascii_tweet.split("\n")[1::]]
-                attributes = ", ".join(attributes)
+        elif comment.id in replied_comments and search:
+            logging.info("We have already replied to comment " + comment.id)
 
-                reply_str = "You have summoned [{0}]({1}) Demon of: {2}"
-                reply = reply_str.format(name, image_url, attributes)
-                comment.reply(reply)
-
-                logging.info("replied to comment id " + comment.id + " with " + reply)
-
-                last_tweet = tweet.id
-                replied_comments.append(comment.id)
-                save_persistent(replied_comments, last_tweet)
-
-            elif comment.id in replied_comments and search:
-                logging.info("We have already replied to comment " + comment.id)
-
-            else:
-                pass
-
-def load_persistent():
-    if not os.path.isfile('last_tweet.p'):
-        last_tweet = ""
-    else:
-        last_tweet = pickle.load(open('last_tweet.p', 'rb'))
-
-    if not os.path.isfile('replied_comments.p'):
-        replied_comments = []
-    else:
-        replied_comments = pickle.load(open('replied_comments.p', 'rb'))
-
-    return (replied_comments, last_tweet)
-
-def save_persistent(replied_comments, last_tweet):
-    pickle.dump(last_tweet, open("last_tweet.p", "wb"))
-    pickle.dump(replied_comments, open("replied_comments.p", "wb"))
-
+        else:
+            pass
 
 def main():
 
